0955348816357158cff851683da2229c3ef80ac2
David Garvey <david.1.bfg@gmail.com>
2015-05-19 18:05:29 -0700

    initialized mastermind solution when site loads

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 Meteor.startup(function () {
-    return Meteor.methods({
-      clearMastermind: function() {
-        return MastermindGuesses.remove({});
-      }
-    });
+  if(0 == MastermindSolution.find({}).count()){
+    var colors = [
+      "red", "green", "orange", "magenta", "blue", "yellow", "brown", "purple"
+    ];
+    var solution = [];
+
+    for(var x = 0; x < 4; x++){
+      solution.push(colors[Math.floor(Math.random() * colors.length)]);
+    }
+
+    MastermindSolution.insert({solution: solution});
+  }
+  
+  return Meteor.methods({
+    clearMastermind: function() {
+      return MastermindGuesses.remove({});
+    }
+  });
 });
